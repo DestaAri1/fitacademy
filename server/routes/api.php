fa2dd5dcc860e5d6e1b43448bebb5706ba510862
DestaAri1 <destacya@gmail.com>
2023-03-27 10:40:12 +0700

    kirim email fix

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\EmailVerificationController;
+use App\Http\Controllers\API\EmailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api', 'verified')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('email/verification-notification', [EmailVerificationController::class, 'sendVerificationEmail'])->middleware('auth:api');
+Route::get('verify-email/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify')->middleware('auth:api', 'verified');
+
+Route::post('/login', [AuthController::class, 'Login']);
+Route::post('/register', [AuthController::class, 'Register']);
+
+Route::post('/email', [EmailController::class, 'addRequest']);
